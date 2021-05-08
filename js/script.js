a6d97049612b4eefcde34c425da18757dc9a3977
El-Deivid <47388903+El-Deivid@users.noreply.github.com>
2021-05-07 22:28:12 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-$.getJSON('https://mindicador.cl/api', function (data) {
-    var dailyIndicators = data;
-    $("<p/>", {
-        html: 'UF $' + dailyIndicators.uf.valor + "<br>Dólar $" + dailyIndicators.dolar.valor
-    }).appendTo("#apisValores");
-}).fail(function () {
-    console.log('Error al consumir la API!');
-});
